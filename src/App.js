8a5886292bbfa006690f7caa20b0768cd6943ba3
dazmag <iscope09@hotmail.com>
2020-12-30 20:57:30 -0500

    map-search

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import {Switch, Route} from 'react-router-dom'
 
 //Component Imports
 import Home from "./components/Home"
+import Search from "./components/Search"
 //HOC which wraps around other components
 import Layout from "./components/common/Layout"
 
 //CSS imports
 import './css/App.css';
+import './css/site.css';
 
 const App = () => {
   return (
     <Layout>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/search" component={Search}/>
       </Switch>
     </Layout>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,88 @@
+// import React from "react"
+import React, { useRef, useEffect } from "react";
+// import useSWR from "swr";
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmluYXJ5YmVhc3QiLCJhIjoiY2tpbTU3cW8xMHE1ZTJycXJkemdjZThmMSJ9.LUCLnUpyYjcUF48GPUEUVQ';
+
+const Search = () => {
+    
+    const mapboxElRef = useRef(null);
+    
+    useEffect(() => {
+        // You can store the map instance with useRef too
+        const map = new mapboxgl.Map({
+          container: mapboxElRef.current,
+          // temperory styling
+          style: "mapbox://styles/binarybeast/ckjbvkxi151ce1ao2552nc7bn",
+          center: [16, 27], // initial geo location
+          zoom: 1.5 // initial zoom
+        });
+
+        map.addControl(new mapboxgl.NavigationControl());
+  
+    }, []);
+
+    
+    return (
+        <div className="App">
+            <div>
+                <h1>Covid-19 Results</h1>
+            <form>
+                <input placeholder="enter by city/state/country">
+                </input>
+                <button class="btn waves-effect waves-light" type="submit" name="action">
+                    <i class="material-icons right">search</i>
+                </button>
+            </form>
+            </div>
+            
+            
+          <div className="mapContainer">
+            {/* Assigned Mapbox container */}
+            <div className="mapBox" ref={mapboxElRef} />
+          </div>
+        </div>
+      );
+    }
+// class Search extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//         lng: 5,
+//         lat: 34,
+//         zoom: 2
+//         };
+//     }
+
+
+
+    // componentDidMount() {
+    //     const map = new mapboxgl.Map({
+    //     container: this.mapContainer,
+    //     style: 'mapbox://styles/mapbox/streets-v11',
+    //     center: [16, 27], // initial geo location
+    //     zoom: 2
+    //     });
+    //     map.on('move', () => {
+    //         this.setState({
+    //         lng: map.getCenter().lng.toFixed(4),
+    //         lat: map.getCenter().lat.toFixed(4),
+    //         zoom: map.getZoom().toFixed(2)
+    //         });
+    //         });
+    //     }
+    //     render() {
+    //         return (
+    //         <div>
+    //             <h1>Covid Results</h1>
+    //         <div className='sidebarStyle'>
+    //         <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+    //         </div>
+    //         <div ref={el => this.mapContainer = el} className='mapContainer' />
+    //         </div>
+    //         )
+    //         }
+    //         }
+export default Search
+
