608a5f9ea8ffd3c49dc5dbe6f00610c6a48a2643
Elizabeth Guerra <ecguerra@outlook.com>
2021-01-01 18:17:33 -0500

    added primarylocation buttons to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react'
-import { getFavorites, getHistory } from '../services/user.service'
+import Form from 'react-validation/build/form'
+import Input from 'react-validation/build/input'
+import CheckButton from 'react-validation/build/button'
+import { getFavorites, getHistory, editPrimary } from '../services/user.service'
 import { getCurrentUser } from '../services/auth.service'
 
 const Dashboard = () => {
@@ -16,20 +19,75 @@ const Dashboard = () => {
         }
       }, [])
 
+    const updatePrimary = (id,city,state,country,county) => {
+        editPrimary(id,city,state,country,county)
+    }
+
     return(
         <>
+        {currentUser ? (
+        <div>
+            <h1>Dashboard - {currentUser.username}</h1>
+            {console.log(currentUser)}
+            {currentUser.primaryLocation ? (
+                <div>
+                    <h3>My Primary Location</h3>
+                    <div>
+                        <h4>{currentUser.primaryLocation.city}, {currentUser.primaryLocation.state} - {currentUser.primaryLocation.country}</h4>
+                    </div>
+                </div>
+            ) : (
+                <div>No Primary Location set</div>
+            )}
+        </div>
+        ) : (
+            <div>Loading...</div>
+        )}
         {userDashboard ? (
         <div>
-            Dashboard
             {/* {console.log(userDashboard)} */}
             {userDashboard.length > 0 ? (
                 <div>
                 <h2>My Locations</h2>
-                <ul>
+                <div>
                     {userDashboard.map(favorite=> (
-                        <li key={favorite._id}>{favorite.city}, {favorite.state}, {favorite.country}</li>
+                        <div key={favorite._id}>
+                            <h4>{favorite.city}, {favorite.state} - {favorite.country}</h4>
+                            <Form onSubmit={updatePrimary}>
+                                <Input 
+                                    type='hidden'
+                                    value={favorite._id}
+                                    name='id'
+                                />
+                                <Input 
+                                    type='hidden'
+                                    value={favorite.city}
+                                    name='city'
+                                />
+                                <Input 
+                                    type='hidden'
+                                    value={favorite.state}
+                                    name='state'
+                                />
+                                <Input 
+                                    type='hidden'
+                                    value={favorite.country}
+                                    name='country'
+                                />
+                                <Input 
+                                    type='hidden'
+                                    value={favorite.county}
+                                    name='county'
+                                />
+                                <Input 
+                                    type='submit'
+                                    value='Set as Primary Location'
+                                    name='submit'
+                                />
+                            </Form>
+                        </div>
                     ))}
-                </ul>
+                </div>
                 </div>
             ) : (
                 <div>No Favorites to Display!</div>
@@ -41,12 +99,12 @@ const Dashboard = () => {
         {searchHistory ? (
         <div>
             Search History
-            {console.log(searchHistory)}
+            {/* {console.log(searchHistory)} */}
             {searchHistory.length > 0 ? (
                 <div>
                 <ul>
-                    {searchHistory.map(history=> (
-                        <li key={history._id}>{history.city}, {history.state}, {history.country}</li>
+                    {searchHistory.map((history, index)=> (
+                        <li key={index}>{history.city}, {history.state}, {history.country}</li>
                     ))}
                 </ul>
                 </div>
